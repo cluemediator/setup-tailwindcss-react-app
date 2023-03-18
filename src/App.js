e6f251a94e9027983b4868e221bede63e509d6a8
Clue Mediator <cluemediator@gmail.com>
2023-03-18 11:46:46 +0530

    Implement demo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1 class="text-3xl hover:text-pink-600">
+      Getting started with Tailwind CSS in React - Clue Mediator
+    </h1>
+  );
+}
+
+export default App;
